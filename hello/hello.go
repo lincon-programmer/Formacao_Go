e58307d25820b062b9ae43a2b4302d596f9af626
Lincon_programmer <lincongallo@hotmail.com>
2020-09-23 16:08:50 -0300

    Trabalhando com variáveis

--- hello/hello.go
+++ hello/hello.go
@@ -3,5 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Olá Mundo! Lincon")
+	nome := "Lincon"
+	idade := 23
+	versao := 1.1
+	fmt.Println("Olá, sr.", nome, "Sua idade é", idade)
+	fmt.Println("Este programa está na versão", versao)
+
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("3- Sair do programa")
+
+	var comando int
+	fmt.Scan(&comando)
+
+	fmt.Println("O endereço da minha variável comando é", &comando)
+	fmt.Println("O valor escolhido foi", comando)
 }
