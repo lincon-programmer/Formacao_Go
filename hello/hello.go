f1218e930d61c8f8c6d519ce3b3eae8b51390cc8
Lincon_programmer <lincongallo@hotmail.com>
2020-09-23 22:21:43 -0300

    Fazendo requisições para a web

--- hello/hello.go
+++ hello/hello.go
@@ -2,25 +2,28 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 )
 
 func main() {
 	exibeIntroducao()
-	exibeMenu()
-	comando := leComando()
+	for {
+		exibeMenu()
+		comando := leComando()
 
-	switch comando {
-	case 1:
-		fmt.Println("Monitorando...")
-	case 2:
-		fmt.Println("Exbindo Logs...")
-	case 0:
-		fmt.Println("Saindo do programa")
-		os.Exit(0)
-	default:
-		fmt.Println("Não conheço este comando!!")
-		os.Exit(-1)
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exbindo Logs...")
+		case 0:
+			fmt.Println("Saindo do programa")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço este comando!!")
+			os.Exit(-1)
+		}
 	}
 }
 
@@ -43,4 +46,16 @@ func leComando() int {
 	fmt.Scan(&comandoLido)
 	fmt.Println("O valor escolhido foi", comandoLido)
 	return comandoLido
+}
+
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando...")
+	site := "https://random-status-code.herokuapp.com/"
+	resp, _ := http.Get(site)
+
+	if resp.StatusCode == 200 {
+		fmt.Println("Site", site, "foi carregado com sucesso")
+	} else {
+		fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+	}
 }
