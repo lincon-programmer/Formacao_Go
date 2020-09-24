b028dc06ada2887412ffb9ad380407c867f4ec99
Lincon_programmer <lincongallo@hotmail.com>
2020-09-23 23:19:21 -0300

    As principais coleções de go

--- hello/hello.go
+++ hello/hello.go
@@ -4,7 +4,11 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 )
+
+const monitoramentos = 3
+const delay = 5
 
 func main() {
 	exibeIntroducao()
@@ -45,12 +49,30 @@ func leComando() int {
 	var comandoLido int
 	fmt.Scan(&comandoLido)
 	fmt.Println("O valor escolhido foi", comandoLido)
+	fmt.Println()
+
 	return comandoLido
 }
 
 func iniciarMonitoramento() {
 	fmt.Println("Monitorando...")
-	site := "https://random-status-code.herokuapp.com/"
+	sites := []string{"https://random-status-code.herokuapp.com/", "https://www.alura.com.br", "https://www.caelum.com.br"}
+
+	for i := 0; i < monitoramentos; i++ {
+		for i, sites := range sites {
+			fmt.Println("Testando site", i, ":", sites)
+			testaSite(sites)
+		}
+		time.Sleep(delay * time.Second)
+		fmt.Println()
+	}
+
+	fmt.Println()
+
+}
+
+func testaSite(site string) {
+
 	resp, _ := http.Get(site)
 
 	if resp.StatusCode == 200 {
